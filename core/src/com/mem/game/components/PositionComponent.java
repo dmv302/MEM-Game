11ca3b9ffe669231c5f16c4152d5a0990e6bf4cd
mikharkiv <mykhailo.postnikov@ukma.edu.ua>
2019-12-07 20:33:52 +0200

    [component] Added BodyComponent, refactored PositionComponent

--- core/src/com/mem/game/components/PositionComponent.java
+++
@@ -1,8 +0,0 @@
-package com.mem.game.components;
-
-import com.badlogic.ashley.core.Component;
-
-public class PositionComponent implements Component {
-	public double x = 0;
-	public double y = 0;
-}
