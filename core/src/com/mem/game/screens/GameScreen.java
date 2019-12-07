67067c2d70f9685ca4452727f6aea3234bb0d21c
brtvsk <vadim.nakitnyak@gmail.com>
2019-12-07 20:55:07 +0200

    [MAP] Game class changed and screens added

---
+++ core/src/com/mem/game/screens/GameScreen.java
@@ -0,0 +1,72 @@
+package com.mem.game.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mem.game.map.WorldMap;
+import com.mem.game.utils.Constants;
+
+public class GameScreen extends MemScreen {
+    SpriteBatch batch;
+    Texture img;
+
+    WorldMap map;
+    OrthographicCamera cam;
+
+    public GameScreen(Game game){
+        super(game);
+        batch = new SpriteBatch();
+        map = new WorldMap();
+        cam = new OrthographicCamera(Constants.VIRTUAL_WIDTH,Constants.VIRTUAL_HEIGHT);
+        cam.position.x = 0;
+        cam.position.y = 0;
+
+        img = new Texture("hero.png");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        batch.setProjectionMatrix(cam.combined);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+        map.render(cam);
+        batch.begin();
+        batch.draw(img,cam.position.x - img.getWidth()/2f,cam.position.y - img.getHeight()/2f);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+}
---
+++ core/src/com/mem/game/screens/MemScreen.java
@@ -0,0 +1,36 @@
+package com.mem.game.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+public abstract class MemScreen implements Screen {
+    Game game;
+
+    public MemScreen(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
