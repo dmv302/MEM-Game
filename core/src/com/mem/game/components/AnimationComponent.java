1d4ae4d60918d787dcdf6abd171b79bf7e023e24
mikharkiv <mykhailo.postnikov@ukma.edu.ua>
2019-12-08 05:30:41 +0200

    [ANIMATION] Player animation and other objects animations added

---
+++ core/src/com/mem/game/components/AnimationComponent.java
@@ -0,0 +1,8 @@
+package com.mem.game.components;
+
+import com.badlogic.ashley.core.Component;
+import com.mem.game.utils.Animation;
+
+public class AnimationComponent implements Component {
+	public Animation animation;
+}
--- core/src/com/mem/game/systems/PlayerSystem.java
+++ core/src/com/mem/game/systems/PlayerSystem.java
@@ -2,7 +2,10 @@ package com.mem.game.systems;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.mem.game.components.PlayerComponent;
+import com.mem.game.components.TextureComponent;
 import com.mem.game.components.TransformComponent;
 import com.mem.game.components.VelocityComponent;
 
@@ -18,6 +21,8 @@ public class PlayerSystem extends EntitySystem {
 		PlayerComponent pc = player.getComponent(PlayerComponent.class);
 		TransformComponent pt = player.getComponent(TransformComponent.class);
 		VelocityComponent vc = player.getComponent(VelocityComponent.class);
+		TextureComponent txc = player.getComponent(TextureComponent.class);
+		// update position
 		if (pc.state == PlayerComponent.StatesEnum.MOVING) {
 			switch (pc.dir) {
 				case DOWN:
@@ -33,6 +38,21 @@ public class PlayerSystem extends EntitySystem {
 					pt.position.x += vc.velocityX * delta;
 					break;
 			}
+		}
+		// update textures
+		switch (pc.dir) {
+			case DOWN:
+				txc.texture = pc.southAnimation.getForTime(pt.time * pc.state.ordinal());
+				break;
+			case UP:
+				txc.texture = pc.northAnimation.getForTime(pt.time * pc.state.ordinal());
+				break;
+			case LEFT:
+				txc.texture = pc.westAnimation.getForTime(pt.time * pc.state.ordinal());
+				break;
+			case RIGHT:
+				txc.texture = pc.eastAnimation.getForTime(pt.time * pc.state.ordinal());
+				break;
 		}
 	}
 	
