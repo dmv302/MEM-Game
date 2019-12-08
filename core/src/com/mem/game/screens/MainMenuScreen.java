c173ce1190a20e6d6131bddebc4676387326f380
mikharkiv <mykhailo.postnikov@ukma.edu.ua>
2019-12-08 09:08:31 +0200

    [DIV] A little fix

--- core/src/com/mem/game/screens/MainMenuScreen.java
+++ core/src/com/mem/game/screens/MainMenuScreen.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mem.game.Game;
+import com.mem.game.utils.Constants;
 
 public class MainMenuScreen extends MemScreen {
 	private Stage ui;
