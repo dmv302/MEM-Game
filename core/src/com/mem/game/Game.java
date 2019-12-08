a6b9dc9561025573afa769c07999802d8c45a1ca
mikharkiv <mykhailo.postnikov@ukma.edu.ua>
2019-12-08 02:48:28 +0200

    [MAIN_MENU] Main menu screen and Skin added

--- core/src/com/mem/game/Game.java
+++ core/src/com/mem/game/Game.java
@@ -1,12 +1,35 @@
 package com.mem.game;
 
 import com.mem.game.screens.GameScreen;
+import com.mem.game.screens.MainMenuScreen;
 
 public class Game extends com.badlogic.gdx.Game {
-
+	private MainMenuScreen mms;
+	private GameScreen gs;
+	
+	private enum StatesEnum { MAIN_MENU, PLAYING, EXITING};
+	private StatesEnum state;
+	
 	@Override
 	public void create () {
-		setScreen(new GameScreen(this));
+		state = StatesEnum.MAIN_MENU;
+		
+		mms = new MainMenuScreen(this);
+		gs = new GameScreen(this);
+		
+		setScreen(mms);
+	}
+	
+	public void play() {
+		if (state == StatesEnum.MAIN_MENU) {
+			setScreen(gs);
+			state = StatesEnum.PLAYING;
+		}
+	}
+	
+	public void exit() {
+		state = StatesEnum.EXITING;
 	}
 
+	
 }
